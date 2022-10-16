20d1ab6ccb44bff1f728a9164cb155fd0931834a
Peter Kosyh <pkosyh@yandex.ru>
2022-10-16 20:59:06 +0300

    fps

--- data/core/core.lua
+++ data/core/core.lua
@@ -2,22 +2,22 @@ local fps = 1/30;
 
 local core = {
 	pal = {
-		{ 0, 0, 0 },
-		{ 0x1D, 0x2B, 0x53 },
-		{ 0x7E, 0x25, 0x53 },
-		{ 0x00, 0x87, 0x51 },
-		{ 0xAB, 0x52, 0x36 },
-		{ 0x5F, 0x57, 0x4F },
-		{ 0xC2, 0xC3, 0xC7 },
-		{ 0xFF, 0xF1, 0xE8 },
-		{ 0xFF, 0x00, 0x4D },
-		{ 0xFF, 0xA3, 0x00 },
-		{ 0xFF, 0xEC, 0x27 },
-		{ 0x00, 0xE4, 0x36 },
-		{ 0x29, 0xAD, 0xFF },
-		{ 0x83, 0x76, 0x9C },
-		{ 0xFF, 0x77, 0xA8 },
-		{ 0xFF, 0xCC, 0xAA },
+		[0] = { 0, 0, 0 },
+		[1] = { 0x1D, 0x2B, 0x53 },
+		[2] = { 0x7E, 0x25, 0x53 },
+		[3] = { 0x00, 0x87, 0x51 },
+		[4] = { 0xAB, 0x52, 0x36 },
+		[5] = { 0x5F, 0x57, 0x4F },
+		[6] = { 0xC2, 0xC3, 0xC7 },
+		[7] = { 0xFF, 0xF1, 0xE8 },
+		[8] = { 0xFF, 0x00, 0x4D },
+		[9] = { 0xFF, 0xA3, 0x00 },
+		[10] = { 0xFF, 0xEC, 0x27 },
+		[11] = { 0x00, 0xE4, 0x36 },
+		[12] = { 0x29, 0xAD, 0xFF },
+		[13] = { 0x83, 0x76, 0x9C },
+		[14] = { 0xFF, 0x77, 0xA8 },
+		[15] = { 0xFF, 0xCC, 0xAA },
 	}
 }
 
@@ -31,6 +31,11 @@ local conf = {
 }
 
 local env = {
+	table = table,
+	math = math,
+	string = string,
+	pairs = pairs,
+	ipairs = ipairs,
 }
 
 function core.err(fmt, ...)
@@ -108,9 +113,15 @@ end
 function env.print(text, x, y, col)
 	x = x or 0
 	y = y or 0
-	core.print(tostring(text),
-		x * conf.font_sz,
-		y * conf.font_sz, col)
+	core.print(tostring(text), x, y, col)
+end
+
+function env.printf(x, y, fmt, ...)
+	return env.print(string.format(fmt, ...), x, y)
+end
+
+function env.time()
+	return system.time()
 end
 
 function env.screen(w, h)
@@ -123,6 +134,11 @@ end
 
 function env.bg(col)
 	conf.bg = core.pal[col] or conf.bg
+end
+
+function env.pixel(x, y, col)
+	col = core.pal[col] or conf.fg
+	return env.win:pixel(x, y, col)
 end
 
 function env.fill(x, y, w, h, col)
@@ -210,7 +226,7 @@ function core.run()
 			end
 		end
 		local elapsed = system.time() - start
-		system.wait(math.max(0, fps - elapsed))
+		system.wait(0) -- math.max(0, fps - elapsed))
 	end
 end
 
--- test.lua
+++ test.lua
@@ -1,9 +1,33 @@
-fg(0)
-bg(15)
-clear()
-print "HELLO WORLD!"
-for i=1,16 do
-	sleep (0.1)
-	print(i, 0, i)
+fg(15)
+bg(0)
+
+stars = {}
+
+for i=1, 128 do
+	table.insert(stars, {
+		x = math.random(512),
+		y = math.random(512),
+		c = math.random(16),
+		s = math.random(8),
+	})
 end
-flip()
+
+local fps = 0
+local start = time()
+local frames = 0
+while true do
+	local cur = time()
+	fps = math.floor(frames / (cur - start))
+	clear(0)
+	printf(0, 0, "FPS: %d", fps)
+	for k, v in ipairs(stars) do
+		pixel(v.x, v.y, v.c)
+		stars[k].y = v.y + v.s
+		if stars[k].y > 512 then
+			stars[k].y = 0
+		end
+	end
+	flip()
+	sleep(1/50 - (time() - cur))
+	frames = frames + 1
+end
