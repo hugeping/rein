a781e7028d63c8f2988b79a3d8ef5f339495bf01
Peter Kosyh <pkosyh@yandex.ru>
2022-10-17 16:27:12 +0300

    bitmap font

---
+++ data/core/font.lua
@@ -0,0 +1,124 @@
+local font = {
+}
+local fn = {
+}
+
+fn.__index = fn
+
+local function parse_line(l)
+	local r = { }
+	for i=1,l:len() do
+		local c = string.byte(l, i)
+		if c == string.byte '-' or c == string.byte ' ' then
+			table.insert(r, 0)
+		else
+			table.insert(r, 255)
+		end
+	end
+	return r
+end
+
+function font.new(fname)
+	local fnt = { w = 0, h = 0 }
+	local f, e = io.open(fname, "rb")
+	if not f then
+		return false, e
+	end
+	local nr = 1
+	local cp = false
+	for l in f:lines() do
+		l = l:gsub("\r", "")
+		if cp then
+			if l:find("^[ \t]*$") then
+				cp = false
+			else
+				local r = parse_line(l)
+				if fnt[cp].w < #r then
+					fnt[cp].w = #r
+				end
+				fnt[cp].h = fnt[cp].h + 1
+				table.insert(fnt[cp], r)
+			end
+		elseif l:find("^[ \t]*0x[0-9a-fA-F]+[ \t]*$") then
+			cp = l:gsub("^[ \t]*", ""):gsub("[ \t]*$", "")
+			cp = tonumber(cp)
+			if not cp then
+				return false, "Error in format. Line:"..tostring(nr)
+			end
+			fnt[cp] = { w = 0, h = 0 }
+		end
+		nr = nr + 1
+	end
+	for k, v in pairs(fnt) do
+		if type(k) == 'number' then
+			if v.w > fnt.w then
+				fnt.w = v.w
+			end
+			if v.h > fnt.h then
+				fnt.h = v.h
+			end
+			v.spr = gfx.new(v.w, v.h)
+		end
+	end
+	setmetatable(fnt, fn)
+	return fnt
+end
+
+function fn:size(t)
+	local w, h = 0, 0
+	while t and t ~= '' do
+		local c, n = system.utf_sym(t, i)
+		if not n or n == 0 then
+			break
+		end
+		local cp = system.utf_codepoint(c)
+		local v = self[cp]
+		if v then
+			w = w + v.w
+			if v.h > h then
+				h = v.h
+			end
+		end
+		t = t:sub(n + 1)
+	end
+	return w, h
+end
+
+function fn:glyph(cp, color)
+	local col =  { color[1], color[2], color[3], color[4] or 0xff }
+	local v = self[cp]
+	if not v then return end
+	v.spr:clear { 0, 0, 0, 0 }
+	for y=1,v.h do
+		for x=1,v.w do
+			col[4] = v[y][x] or 0
+			v.spr:pixel(x - 1, y - 1, col)
+		end
+	end
+	return v.spr
+end
+
+function fn:text(t, color)
+	local w, h = self:size(t)
+	local spr = gfx.new(w, h)
+	local x, y = 0, 0
+	while t and t ~= '' do
+		local c, n = system.utf_sym(t, i)
+		if not n or n == 0 then
+			break
+		end
+		local cp = system.utf_codepoint(c)
+		if n > 1 then
+--			print(string.format("%x\n", cp))
+		end
+		local v = self[cp]
+		if v then
+			self:glyph(cp, color):copy(spr, x, 0)
+			x = x + v.w
+		end
+		t = t:sub(n + 1)
+	end
+	return spr
+end
+
+return font
--- test.lua
+++ test.lua
@@ -1,12 +1,13 @@
 fg(15)
 bg(0)
-
+local w = 256
+local h = 256
 stars = {}
 
 for i=1, 128 do
 	table.insert(stars, {
-		x = math.random(512),
-		y = math.random(512),
+		x = math.random(w),
+		y = math.random(h),
 		c = math.random(16),
 		s = math.random(8),
 	})
@@ -21,13 +22,14 @@ while true do
 	fps = math.floor(frames / (cur - start))
 	clear(0)
 	local mx, my, mb = mouse()
-	printf(0, 0, "FPS: %d\nMouse: %d, %d %s",
+	printf(0, 0, "FPS: %d\nМышь: %d, %d %s",
 		fps, mx, my, mb.left and 'left' or '')
 	for k, v in ipairs(stars) do
 		pixel(v.x, v.y, v.c)
 		stars[k].y = v.y + v.s
-		if stars[k].y > 512 then
+		if stars[k].y > h then
 			stars[k].y = 0
+			stars[k].x = math.random(w)
 		end
 	end
 	flip(1/50)
