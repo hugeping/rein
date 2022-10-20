8069e0302dbb23f5be0d682a74d3596b142d4e8b
Peter Kosyh <pkosyh@yandex.ru>
2022-10-20 13:41:34 +0300

    mixer: split

--- data/core/api.lua
+++ data/core/api.lua
@@ -1,5 +1,6 @@
 local font = require "font"
 local spr = require "spr"
+local mixer = require "mixer"
 
 local core
 local input = {
@@ -339,19 +340,6 @@ function env_ro.sprite(x, y)
 	end
 end
 
-function env_ro.audio(t)
-	local idx = 1
-	local rc
-	while true do
-		rc = system.audio(t, idx)
-		if #t == rc + idx - 1 then
-			return
-		end
-		idx = idx + rc
-		coroutine.yield()
-	end
-end
-
 function env_ro.thread(fn)
 	local f, e = coroutine.create(fn)
 	if f then
@@ -364,79 +352,7 @@ function env_ro.yield(...)
 	return coroutine.yield(...)
 end
 
-local mixer = {
-	chans = {};
-}
-
-function mixer.thread()
-	while true do
-		local t = {}
-		for i = 1,4096,2 do
-			local mix = {}
-			local k, n = 1, #mixer.chans
-			local ll, rr = 0, 0
-			while k<=n do
-				local fn = mixer.chans[k]
-				local st, l, r = coroutine.resume(fn)
-				if not st or not l then
-					table.remove(mixer.chans, k)
-					n = n - 1
-					if not st then
-						core.err(l..'\n'..debug.traceback(fn))
-					end
-				else
-					k = k + 1
-					ll = ll + l
-					rr = rr + r
-				end
-			end
-			if mixer.chans[1] then
-				t[i] = ll / #mixer.chans
-				t[i+1] = rr / #mixer.chans
-			else
-				break
-			end
-		end
-		if t[1] then
-			env.audio(t)
-		end
-		coroutine.yield()
-	end
-end
-
 env_ro.mixer = mixer
-
-function mixer.check(fn)
-	if not fn then
-		return #mixer.chans > 0
-	end
-	for k, v in ipairs(mixer.chans) do
-		if v == fn then
-			return true
-		end
-	end
-end
-
-function mixer.stop(fn)
-	if not fn then
-		mixer.chans = {}
-	end
-	for k, v in ipairs(mixer.chans) do
-		if v == fn then
-			table.remove(mixer.chans, k)
-			return
-		end
-	end
-end
-
-function mixer.add(fn)
-	local f, e = coroutine.create(fn)
-	if not f then
-		return f, e
-	end
-	table.insert(mixer.chans, f)
-	return f
-end
 
 local api = {}
 
