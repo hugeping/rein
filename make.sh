2e548af355b06a96db59d317831d0f2dfa0b2e09
Peter Kosyh <pkosyh@yandex.ru>
2022-10-26 15:35:56 +0300

    removed useless gcc flag

--- make.sh
+++ make.sh
@@ -1,4 +1,4 @@
-CFLAGS="`pkg-config --cflags sdl2` `pkg-config --cflags luajit`-Dunix"
+CFLAGS="`pkg-config --cflags sdl2` `pkg-config --cflags luajit`"
 LDFLAGS="`pkg-config --libs sdl2` `pkg-config --libs luajit` -lm"
 gcc -Wall -O3 src/*.c $CFLAGS $LDFLAGS -o dein
 rm -f *.o
