9f68029613df748a2d22e3bfdfd95724a827baee
Peter Kosyh <pkosyh@yandex.ru>
2022-10-26 15:33:29 +0300

    goodbye lua

--- make-default.sh
+++
@@ -1,4 +0,0 @@
-CFLAGS="`sdl2-config --cflags` -Isrc/lua -Dunix"
-LDFLAGS="`sdl2-config --libs` -lm"
-gcc -Wall -O3 src/*.c src/lua/*.c $CFLAGS $LDFLAGS -o dein
-rm -f *.o
--- make-tcc.sh
+++
@@ -1,4 +0,0 @@
-CFLAGS="`pkg-config --cflags sdl2` -Isrc/lua -Dunix -DSDL_DISABLE_IMMINTRIN_H -DSTBI_NO_SIMD"
-LDFLAGS="`pkg-config --libs sdl2` -lm"
-tcc -o dein src/*.c src/lua/*.c $CFLAGS $LDFLAGS
-rm -f *.o
