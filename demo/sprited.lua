6937a6e6f24c16faeec8815ffb3414c73950601b
Peter Kosyh <pkosyh@yandex.ru>
2022-10-28 12:04:38 +0300

    sprited: tools

--- demo/sprited.lua
+++ demo/sprited.lua
@@ -6,71 +6,8 @@ local spr = {}
 local pan_mode
 
 local SPRITE = ARGS[2] or 'sprite.spr'
-spr.C = gfx.new [[
--------*
---*--
------
-*-*-*
------
---*--
-]]
-
-spr.HL = gfx.new [[
---------89------
---------
--88--88-
--8----8-
----99---
----99---
--8----8-
--88--88-
---------
-]]
-
-spr.Hand = gfx.new [[
----------------f
---fff---
---ffff--
---ffff--
-f-fffff-
-fffffff-
-fffffff-
--fffff--
---ffff--
-]]
-
-spr.X = gfx.new [[
-*
-*---*
--*-*-
---*--
--*-*-
-*---*
-]]
-
-spr.G = gfx.new [[
---------------e-
---e--e--
---e--e--
-eeeeeeee
---e--e--
---e--e--
-eeeeeeee
---e--e--
---e--e--
-]]
-
-spr.S = gfx.new [[
-------------c---
---------
--cc--cc-
--c----c-
---------
---------
--c----c-
--cc--cc-
---------
-]]
+local COLORS = 16
+local HCOLORS = COLORS/2
 
 pal = {
 	x = 0;
@@ -78,7 +15,7 @@ pal = {
 	cw = 8;
 	ch = 8;
 	w = 8*2;
-	h = 8*8 + 16;
+	h = (HCOLORS + 4)*8;
 	color = 0;
 	lev = -1;
 }
@@ -97,44 +34,58 @@ local grid_mode = true
 local sel_mode = false
 local hand_mode = false
 local hl_mode = false
+local l_mode = false
+local b_mode = false
+local c_mode = false
 
 function pal:show()
 	local s = self
 	local w = s.cw
 	local h = s.ch
 	local x, y = self.x, self.y
-	for y=0, 7 do
+	for y=0, HCOLORS-1 do
 		screen:clear(x, y*h, w, h, y)
-		screen:clear(x+w, y*h, w, h, y+8)
+		screen:clear(x+w, y*h, w, h, y+HCOLORS)
 	end
 	local n = s.color
-	local c = n + 7
-	if c >= 16 then c = c - 16 end
-	if n >= 8 then
+	local c = n + (HCOLORS-1)
+	if c >= COLORS then c = c - COLORS end
+	if n >= HCOLORS then
 		x = x + w
-		n = n - 8
+		n = n - HCOLORS
 	end
 	y = n
 	screen:poly({x, y*h,
 		x + w - 1, y*h,
 		x + w -1, y*h + h - 1,
 		x, y*h + h - 1}, c)
+	local py = HCOLORS
 	if hand_mode then
-		self:select(0, 8, 7)
+		self:select(0, py, 7)
 	end
 	if grid_mode then
-		self:select(1, 8, 10)
+		self:select(1, py, 10)
 	end
 	if sel_mode then
-		self:select(0, 9, 7)
+		self:select(0, py+1, 7)
 	end
 	if hl_mode then
-		self:select(1, 9, 10)
+		self:select(1, py+1, 10)
 	end
-	spr.Hand:blend(screen, s.x, 8*8)
-	spr.G:blend(screen, s.x + 8, 8*8)
-	spr.S:blend(screen, s.x, 8*9)
-	spr.HL:blend(screen, s.x + 8, 8*9)
+	if l_mode then
+		self:select(0, py+2, 8)
+	elseif b_mode then
+		self:select(1, py+2, 8)
+	elseif c_mode then
+		self:select(0, py+3, 8)
+	end
+	spr.Hand:blend(screen, s.x, py*8)
+	spr.G:blend(screen, s.x + 8, py*8)
+	spr.S:blend(screen, s.x, (py+1)*8)
+	spr.HL:blend(screen, s.x + 8, (py+1)*8)
+	spr.L:blend(screen, s.x, (py+2)*8)
+	spr.B:blend(screen, s.x + 8, (py+2)*8)
+	spr.C:blend(screen, s.x, (py+3)*8)
 end
 
 function pal:pos2col(x, y)
@@ -151,20 +102,30 @@ end
 function pal:click(x, y, mb, click)
 	local x, y = self:pos2col(x, y)
 	local c
-	if y < 8 then
-		c = x*8 + y
+	if y < HCOLORS then
+		c = x*HCOLORS + y
 		self.color = c
-	elseif y == 8 and x == 0 and click then -- hand mode
+	elseif y == HCOLORS and x == 0 and click then -- hand mode
 		hand_mode = not hand_mode
-		sel_mode = false
-	elseif y == 8 and x == 1 and click then -- grid mode
+		sel_mode, l_mode, b_mode, c_mode = false, false, false, false
+	elseif y == HCOLORS and x == 1 and click then -- grid mode
 		grid_mode = not grid_mode
-	elseif y == 9 and x == 0 and click then
+	elseif y == HCOLORS+1 and x == 0 and click then
 		sel_mode = not sel_mode
-		hand_mode = false
-	elseif y == 9 and x == 1 and click then
-		hl_mode = not hl_mode
+		l_mode, hand_mode = false, false
+	elseif y == HCOLORS+1 and x == 1 and click then
+		hl_mode = not hl_mod
+	elseif y == HCOLORS+2 and x == 0 and click then
+		l_mode = not l_mode
+		sel_mode, hand_mode, b_mode, c_mode = false, false, false, false
+	elseif y == HCOLORS+2 and x == 1 and click then
+		b_mode = not b_mode
+		sel_mode, hand_mode, l_mode, c_mode = false, false, false, false
+	elseif y == HCOLORS+3 and x == 0 and click then
+		c_mode = not c_mode
+		sel_mode, hand_mode, l_mode, b_mode = false, false, false, false
 	end
+
 	return true
 end
 
@@ -499,7 +460,7 @@ function grid:click(x, y, mb, click)
 	if not x then
 		return
 	end
-	if sel_mode then
+	if sel_mode or l_mode or b_mode or c_mode then
 		if click then
 			if mb.right then
 				self.sel_x1, self.sel_y1 = false, false
@@ -522,10 +483,14 @@ function grid:click(x, y, mb, click)
 	s.pixels[y][x] = nval
 	return true
 end
-function grid:getsel()
+
+function grid:getsel(nosort)
 	local s = self
 	if not s.sel_x1 then
 		return
+	end
+	if nosort then
+		return s.sel_x1, s.sel_y1, s.sel_x2, s.sel_y2
 	end
 	local xmin = math.min(s.sel_x1, s.sel_x2)
 	local ymin = math.min(s.sel_y1, s.sel_y2)
@@ -533,6 +498,126 @@ function grid:getsel()
 	local ymax = math.max(s.sel_y1, s.sel_y2)
 	return xmin, ymin, xmax, ymax
 end
+
+function grid:show_line(x1, y1, x2, y2, c, draw)
+	local s = self
+	local dx = x2 - x1
+	local dy = y2 - y1
+	local steps = math.max(math.max(math.abs(dx), math.abs(dy)), 1)
+	local x_step = dx / steps
+	local y_step = dy / steps
+	local dd = floor(s.w / s.grid)
+	if draw then
+		local xmin = math.min(x1, x2)
+		local ymin = math.min(y1, y2)
+		local xmax = math.max(x1, x2)
+		local ymax = math.max(y1, y2)
+		s:histadd(xmin, ymin, xmax, ymax)
+	end
+	for i = 0, steps do
+		local x, y = math.round(x1) - s.xoff - 1, math.round(y1) - s.yoff - 1
+		screen:clear(x*dd, y*dd, dd, dd, c)
+		if draw then
+			x, y = math.round(x1), math.round(y1)
+			s.pixels[y] = s.pixels[y] or {}
+			s.pixels[y][x] = c
+		end
+		x1 = x1 + x_step
+		y1 = y1 + y_step
+	end
+end
+
+function grid:show_box(x1, y1, x2, y2, c, draw)
+	local s = self
+	local dd = floor(s.w / s.grid)
+	if draw then
+		s:histadd(x1, y1, x2, y2)
+	end
+	for x=x1,x2 do
+		local xx, yy = x - s.xoff - 1, y1 - s.yoff - 1
+		screen:clear(xx*dd, yy*dd, dd, dd, c)
+		yy = y2 - s.yoff - 1
+		screen:clear(xx*dd, yy*dd, dd, dd, c)
+		if draw then
+			s.pixels[y1] = s.pixels[y1] or {}
+			s.pixels[y1][x] = c
+			s.pixels[y2] = s.pixels[y2] or {}
+			s.pixels[y2][x] = c
+		end
+	end
+	for y=y1,y2 do
+		local xx, yy = x1 - s.xoff - 1, y - s.yoff - 1
+		screen:clear(xx*dd, yy*dd, dd, dd, c)
+		xx = x2 - s.xoff - 1
+		screen:clear(xx*dd, yy*dd, dd, dd, c)
+		if draw then
+			s.pixels[y] = s.pixels[y] or {}
+			s.pixels[y][x1] = c
+			s.pixels[y][x2] = c
+		end
+	end
+end
+
+local function ellipse(x0, y0, x1, y1, pixel)
+	local a, b = x1-x0, y1-y0
+	local b1 = b % 2
+	local dx, dy = 4*(1-a)*b*b, 4*(b1+1)*a*a
+	local err = dx+dy+b1*a*a
+
+	y0 = y0 + floor(0.5*(b + 1))
+	y1 = y0 - b1
+	a = 8*a*a
+	b1 = 8*b*b
+
+	repeat
+		pixel(x1, y0)
+		pixel(x0, y0)
+		pixel(x0, y1)
+		pixel(x1, y1)
+
+		local e2 = err + err
+		if e2 <= dy then
+			y0 = y0 + 1
+			y1 = y1 - 1
+			dy = dy + a
+			err = err + dy
+		end
+		if e2 >= dx or (err + err) > dy then
+			x0 = x0 + 1
+			x1 = x1 - 1
+			dx = dx + b1
+			err = err + dx
+		end
+	until x0 > x1
+
+	while y0 - y1 < b do
+		pixel(x0 - 1, y0)
+		pixel(x1 + 1, y0)
+		pixel(x0-1, y1)
+		pixel(x1+1, y1)
+
+		y0 = y0 + 1
+		y1 = y1 - 1
+	end
+end
+
+function grid:show_circle(x1, y1, x2, y2, c, draw)
+	local s = self
+	local dd = floor(s.w / s.grid)
+	if draw then
+		s:histadd(x1, y1, x2, y2)
+	end
+	local dd = floor(s.w / s.grid)
+	ellipse(x1, y1, x2, y2, function(x, y)
+		local xx, yy = x - s.xoff - 1, y - s.yoff - 1
+		screen:clear(xx*dd, yy*dd, dd, dd, c)
+		if draw then
+			s.pixels[y] = s.pixels[y] or {}
+			s.pixels[y][x] = c
+		end
+	end)
+end
+
 function grid:show()
 	local s = self
 	local mx, my = input.mouse()
@@ -585,12 +670,21 @@ function grid:show()
 				return
 		end
 		local dx = floor(s.w / s.grid)
-		xmin = xmin - 1
-		ymin = ymin - 1
+		if l_mode then
+			xmin, ymin, xmax, ymax = s:getsel(true)
+			s:show_line(xmin, ymin, xmax, ymax, pal.color)
+		elseif b_mode then
+			s:show_box(xmin, ymin, xmax, ymax, pal.color)
+		elseif c_mode then
+			s:show_circle(xmin, ymin, xmax, ymax, pal.color)
+		end
+		xmin, ymin, xmax, ymax = s:getsel()
+		xmin = xmin - 1; ymin = ymin - 1
 		screen:poly({(xmin - s.xoff)*dx, (ymin - s.yoff)*dx,
 			(xmax - s.xoff)*dx, (ymin - s.yoff)*dx,
 			(xmax - s.xoff)*dx, (ymax - s.yoff)*dx,
 			(xmin - s.xoff)*dx, (ymax - s.yoff)*dx}, 7)
+		--end
 	end
 end
 
@@ -643,6 +737,26 @@ function proc_inp(r, e, a, b, c, d)
 			grid:fliph()
 		elseif e == 'v' then
 			grid:flipv()
+		end
+	elseif r == 'mouseup' then
+		if l_mode then
+			local x1, y1, x2, y2 = grid:getsel(true)
+			if x1 then
+				grid:show_line(x1, y1, x2, y2, pal.color, true)
+				grid.sel_x1 = false
+			end
+		elseif b_mode then
+			local x1, y1, x2, y2 = grid:getsel()
+			if x1 then
+				grid:show_box(x1, y1, x2, y2, pal.color, true)
+				grid.sel_x1 = false
+			end
+		elseif c_mode then
+			local x1, y1, x2, y2 = grid:getsel()
+			if x1 then
+				grid:show_circle(x1, y1, x2, y2, pal.color, true)
+				grid.sel_x1 = false
+			end
 		end
 	end
 	if not pan_mode and (r == 'keydown' and  e == 'space' or
@@ -697,6 +811,8 @@ mmb - middle mouse button
 [filename] - on the status line
 [scale]    - on the status line
 ]]
+
+function run()
 while true do
 	local r, v, a, b = sys.input()
 	local mx, my, mb = input.mouse()
@@ -732,3 +848,110 @@ while true do
 	end
 		gfx.flip(1, true) -- wait for event
 end
+end
+
+spr.L = gfx.new [[
+------6------d--
+dddddddd
+d-----6d
+d----6-d
+d---6--d
+d--6---d
+d-6----d
+d6-----d
+dddddddd
+]]
+
+spr.B = gfx.new [[
+------6------d--
+66666666
+6------6
+6------6
+6------6
+6------6
+6------6
+6------6
+66666666
+]]
+
+spr.C = gfx.new [[
+------6------d--
+dddddddd
+d--66--d
+d-6--6-d
+d6----6d
+d6----6d
+d-6--6-d
+d--66--d
+dddddddd
+]]
+
+--[==[
+spr.C = gfx.new [[
+-------*
+--*--
+-----
+*-*-*
+-----
+--*--
+]]
+]==]--
+
+spr.HL = gfx.new [[
+--------89------
+--------
+-88--88-
+-8----8-
+---99---
+---99---
+-8----8-
+-88--88-
+--------
+]]
+
+spr.Hand = gfx.new [[
+---------------f
+--fff---
+--ffff--
+--ffff--
+f-fffff-
+fffffff-
+fffffff-
+-fffff--
+--ffff--
+]]
+
+spr.X = gfx.new [[
+*
+*---*
+-*-*-
+--*--
+-*-*-
+*---*
+]]
+
+spr.G = gfx.new [[
+--------------e-
+--e--e--
+--e--e--
+eeeeeeee
+--e--e--
+--e--e--
+eeeeeeee
+--e--e--
+--e--e--
+]]
+
+spr.S = gfx.new [[
+------------c---
+--------
+-cc--cc-
+-c----c-
+--------
+--------
+-c----c-
+-cc--cc-
+--------
+]]
+
+run()
