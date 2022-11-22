b9c867284047e21532d63b469eb2dd1e57af859b
Peter Kosyh <pkosyh@yandex.ru>
2022-11-22 14:42:06 +0300

    synth: voices

--- data/core/mixer.lua
+++ data/core/mixer.lua
@@ -5,6 +5,8 @@ local THREADED = true
 
 local mixer = {
   id = 0;
+  ids = {};
+  voices = {};
   vol = 0.5;
   req = { };
   ack = { };
@@ -32,15 +34,79 @@ function mixer.get_channels(nr)
   return free
 end
 
+function mixer.apply_voice(chan, voice)
+  voice = mixer.voices[voice] or error("No such voice: "..tostring(voice))
+  local stack = 0
+  synth.free(chan)
+  for _, v in ipairs(voice) do
+    if type(v) == 'string' then
+      stack = synth.push(chan, v)
+    elseif type(v) == 'table' then
+      synth.change(chan, stack, table.unpack(v))
+    else
+      error("Wrong voice format: "..tostring(voice))
+    end
+  end
+end
+
 function mixer.setup_channels(chans, voices)
   for i, v in ipairs(voices) do
-    synth.free(chans[i])
-    synth.push(chans[i], v)
+    mixer.apply_voice(chans[i], v)
   end
 end
 
 function mixer.req_nextid()
-  mixer.id = (mixer.id + 1) % 0xffff
+  while mixer.ids[mixer.id] do
+    mixer.id = (mixer.id + 1) % 0xffff
+  end
+end
+
+
+function mixer.free_channels(chans)
+  if not chans then
+    for i=1, mixer.chans.size do
+      mixer.chans[i] = false
+    end
+    return
+  end
+  for _, c in ipairs(chans) do
+    mixer.chans[c] = false
+  end
+end
+
+function mixer.release(r)
+  mixer.free_channels(r.chans)
+  mixer.ids[r.id] = nil
+end
+
+function mixer.change()
+  local r, e
+  for i, v in ipairs(mixer.fn) do
+    if coroutine.status(v.fn) ~= 'dead' then
+      r, e = coroutine.resume(v.fn, table.unpack(v.args))
+      if not r then error(e) end
+    else
+      mixer.fn[i].dead = true
+    end
+  end
+  local i = 1
+  while i <= #mixer.fn do -- clean dead coroutines and free chans
+    if mixer.fn[i].dead then
+      r = table.remove(mixer.fn, i)
+      mixer.release(r)
+    else
+      i = i + 1
+    end
+  end
+end
+
+function mixer.proc(tick)
+  local rc
+  repeat
+    rc = synth.mix(tick, mixer.vol)
+    if rc == 0 then sys.sleep(mixer.freq*2) end
+    tick = tick - rc
+  until tick == 0
 end
 
 function mixer.req_play(voices, pans, song, temp, nr)
@@ -58,51 +124,16 @@ function mixer.req_play(voices, pans, song, temp, nr)
   end
   mixer.req_nextid()
   mixer.setup_channels(chans, voices)
-  table.insert(mixer.fn, { id = mixer.id, fn = f, chans = chans,
-    args = { chans, pans, song, temp, nr } })
+  local r = { id = mixer.id, fn = f, chans = chans,
+    args = { chans, pans, song, temp, nr } }
+  table.insert(mixer.fn, r)
+  mixer.ids[mixer.id] = r
   return mixer.id
 end
 
-function mixer.free_channels(chans)
-  if not chans then
-    for i=1, mixer.chans.size do
-      mixer.chans[i] = false
-    end
-    return
-  end
-  for _, c in ipairs(chans) do
-    mixer.chans[c] = false
-  end
-end
-
-function mixer.change()
-  local r, e
-  for i, v in ipairs(mixer.fn) do
-    if coroutine.status(v.fn) ~= 'dead' then
-      r, e = coroutine.resume(v.fn, table.unpack(v.args))
-      if not r then error(e) end
-    else
-      mixer.fn[i].dead = true
-    end
-  end
-  local i = 1
-  while i <= #mixer.fn do -- clean dead coroutines and free chans
-    if mixer.fn[i].dead then
-      r = table.remove(mixer.fn, i)
-      mixer.free_channels(r.chans)
-    else
-      i = i + 1
-    end
-  end
-end
-
-function mixer.proc(tick)
-  local rc
-  repeat
-    rc = synth.mix(tick, mixer.vol)
-    if rc == 0 then sys.sleep(mixer.freq*2) end
-    tick = tick - rc
-  until tick == 0
+function mixer.req_voice(nam, a)
+  mixer.voices[nam] = (#a ~= 0) and a or nil
+  return true
 end
 
 function mixer.getreq()
@@ -130,9 +161,16 @@ function mixer.answer(...)
   end
 end
 
+function mixer.reset()
+  mixer.fn = {}
+  mixer.voices = {}
+  mixer.ids = {}
+end
+
 function mixer.thread()
   print "mixer start"
   local r, v, a, b, c
+  mixer.reset()
   while true do
     r, v, a = mixer.getreq()
     if r == 'quit' then -- stop thread
@@ -144,13 +182,14 @@ function mixer.thread()
       mixer.answer(oval)
     elseif r == 'play' then
       mixer.answer(mixer.req_play(table.unpack(v)))
+    elseif r == 'voice' then
+      mixer.answer(mixer.req_voice(v, a))
     end
     mixer.change()
     mixer.proc(mixer.tick) -- write to audio!
   end
   mixer.free_channels()
   synth.stop()
-  mixer.fn = {}
   print "mixer finish"
 end
 
@@ -198,6 +237,10 @@ end
 
 function mixer.play(...)
   return mixer.clireq("play", {...})
+end
+
+function mixer.voice(name, ...)
+  return mixer.clireq("voice", name, {...})
 end
 
 function mixer.init()
