ba770e16b294d5fc54c13e53bf24da77de543d11
Peter Kosyh <pkosyh@yandex.ru>
2022-12-26 16:52:08 +0300

    ci: emscripten build

--- contrib/make-emscripten.sh
+++ contrib/make-emscripten.sh
@@ -1,5 +1,5 @@
 VERSION=`date +%y%m%d`
-. /home/peter/Devel/emsdk-portable/emsdk/emsdk_env.sh
+# . /home/peter/Devel/emsdk-portable/emsdk/emsdk_env.sh
 emcc -O2 -o rein.html src/*.c src/lua/*.c -Isrc/lua -s USE_SDL=2 -DDATADIR=\"/data\" -DVERSION=\"$VERSION\" \
 -lidbfs.js -s WASM=1 -s SAFE_HEAP=0  -s ALLOW_MEMORY_GROWTH=1 \
 --preload-file data/ \
