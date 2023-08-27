72a3453f68468214afb77559e453e7d31292e69d
Peter Kosyh <pkosyh@yandex.ru>
2023-08-27 15:07:04 +0300

    tiny: input method

--- data/lib/tiny.lua
+++ data/lib/tiny.lua
@@ -14,19 +14,29 @@ keypress = input.keypress
 dprint = print
 max = math.max
 min = math.min
+tostr = tostring
+tonum = tonumber
 
 local px, py = 0, 0
 print = function(t, x, y, c, scroll)
+  t = t or ""
   if scroll == nil then
     scroll = not x
   end
-  px, py = gfx.print(t, x or px, y or py, c or false, scroll)
+  px, py = gfx.print(tostring(t), x or px, y or py, c or false, scroll)
   return px, py
 end
 
-println = function(t, ...)
-  if t ~= nil then t = tostring(t) .. '\n' end
-  return print(t, ...)
+println = function(...)
+  local t = ""
+  for _, v in ipairs {...} do
+    if t ~= '' then t = t .. ' ' end
+    t = t .. tostring(v)
+  end
+  t = t .. '\n'
+  local x, y = print(t)
+  px = 0
+  return x, y
 end
 
 printf = gfx.printf
@@ -58,4 +68,37 @@ for _, n in ipairs { 'clear', 'pixel', 'fill', 'line', 'lineAA',
   'fill_poly', 'poly', 'polyAA', 'fill_rect', 'rect', 'rectAA',
   'clip', 'noclip', 'offset', 'nooffset' } do
   make_wrapper(n)
+end
+
+function input(msg)
+  local t = {}
+  local spw, sph = font:size(" ")
+  local x, y = print(msg or "")
+  clear(x, y, spw, sph, gfx.fg())
+  while sys.running() do
+    local r, v, a = sys.input()
+    if r == 'text' then
+      table.insert(t, v)
+      clear(x, y, spw, sph, gfx.bg())
+      x, y = print(v, x, y)
+      clear(x, y, spw, sph, gfx.fg())
+    elseif r == 'keydown' then
+      if v == 'backspace' then
+        if #t > 0 then
+          clear(x, y, spw, sph, gfx.bg())
+          x = x - spw
+          clear(x, y, spw, sph, gfx.bg())
+          clear(x, y, spw, sph, gfx.fg())
+        end
+        table.remove(t, #t)
+      elseif v == 'return' then
+        clear(x, y, spw, sph, gfx.bg())
+        println()
+        return table.concat(t, '')
+      end
+    end
+    gfx.flip(1, true)
+  end
+  clear(x, y, spw, sph, gfx.bg())
+  return ''
 end
