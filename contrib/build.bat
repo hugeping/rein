e552d20719f61679fbffc450ff11a3e3c897e43b
Peter Sovietov <peter.sovietov@gmail.com>
2023-09-28 13:37:18 +0300

    building script for Windows

--- contrib/build.bat
+++ contrib/build.bat
@@ -1,6 +1,6 @@
 set MINGW=../../mingw64/bin
 set REIN=..
 set LUA=../../LuaJIT
-set SDL=../../SDL2
+set SDL=../../SDL2/x86_64-w64-mingw32
 set CFILES=%REIN%/src/bit.c %REIN%/src/gfx.c %REIN%/src/gfx_font.c %REIN%/src/lua-compat.c %REIN%/src/main.c %REIN%/src/net.c %REIN%/src/platform.c %REIN%/src/stb_image.c %REIN%/src/stb_image_resize.c %REIN%/src/stb_truetype.c %REIN%/src/synth.c %REIN%/src/system.c %REIN%/src/thread.c %REIN%/src/utf.c %REIN%/src/zvon.c %REIN%/src/zvon_mixer.c %REIN%/src/zvon_sfx.c
-"%MINGW%/gcc.exe" -Wall -O3 %CFILES% -I%LUA%/src -I%SDL%/x86_64-w64-mingw32/include/SDL2 -L%LUA%/src -lluajit -L%SDL%/x86_64-w64-mingw32/lib -lSDL2 -lSDL2main -lws2_32 -o ../rein.exe
+"%MINGW%/gcc.exe" -Wall -O3 %CFILES% -I%LUA%/src -I%SDL%/include/SDL2 -L%LUA%/src -lluajit -L%SDL%/lib -lSDL2 -lSDL2main -lws2_32 -o %REIN%/rein.exe
