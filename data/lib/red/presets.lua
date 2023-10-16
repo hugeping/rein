154d15346c31f1937d6cfc09baa6962f09b2f1ee
Peter Kosyh <pkosyh@yandex.ru>
2023-10-16 10:25:53 +0300

    red: cpp is c presets

--- data/lib/red/presets.lua
+++ data/lib/red/presets.lua
@@ -1,5 +1,6 @@
 return {
   {"%.[cChH]$", { ts = 8, spaces_tab = false, trim_spaces = true, syntax = "c" } },
+  {"%.cpp$", { ts = 8, spaces_tab = false, trim_spaces = true, syntax = "c" } },
   {"%.lua$", { ts = 2, spaces_tab = true, trim_spaces = true, syntax = "lua" } },
   {"%.md$", { ts = 2, spaces_tab = true } },
   {"%.go$", { ts = 8, spaces_tab = false, trim_spaces = true } },
