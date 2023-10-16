115ab67d979d70b0d743835f2922289f2b4af409
Peter Kosyh <pkosyh@yandex.ru>
2023-10-16 15:41:13 +0300

    red: color scheme fix

--- data/lib/red/syntax/scheme.lua
+++ data/lib/red/syntax/scheme.lua
@@ -1,7 +1,7 @@
 return {
-  keyword = { 152, 152, 72 },
+  keyword = { 122, 112, 72 },
   comment = { 64, 136, 64 },
-  string = { 184, 192, 87 },
+  string = { 124, 102, 187 },
   number = { 2, 135, 200 },
   operator = { 0x6d, 0x1d, 0x1d },
   lib = { 184, 92, 97 },
