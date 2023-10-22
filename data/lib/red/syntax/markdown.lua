a105bee51e0fcf5353284eff1f91c4a215a60418
Peter Kosyh <pkosyh@yandex.ru>
2023-10-22 12:06:39 +0300

    red: markdown syntax

---
+++ data/lib/red/syntax/markdown.lua
@@ -0,0 +1,81 @@
+local scheme = require "red/syntax/scheme"
+
+local function endsect(ctx, txt, i)
+  if txt[i] == '\n' and txt[i+1] ~= ' ' then
+    return 1
+  end
+end
+
+local function numbersect(ctx, txt, pos)
+  local n = ''
+  for i = pos, #txt do
+    if txt[i]:find("[0-9]") then
+      n = n .. txt[i]
+    elseif txt[i] == '.' then
+      break
+    else
+      return
+    end
+  end
+  return n:len() + 1
+end
+
+local col = {
+  col = 0,
+  keywords = {
+    { '\\*', '\\#', '\\-', },
+  },
+  { -- section
+    linestart = '#',
+    stop = '\n',
+    col = scheme.lib,
+  },
+  { -- items
+    linestart = '*',
+    stop = endsect,
+    col = scheme.number,
+  },
+  { -- items
+    linestart = '-',
+    stop = endsect,
+    col = scheme.number,
+  },
+  { -- items
+    linestart = numbersect,
+    stop = endsect,
+    col = scheme.number,
+  },
+  { -- quote
+    linestart = '>',
+    stop = '\n',
+    col = scheme.comment,
+  },
+  { -- code
+    linestart = '    ',
+    stop = '\n',
+    col = scheme.string,
+  },
+  { -- code
+    linestart = '```',
+    stop = '```',
+    col = scheme.string,
+  },
+  { -- strong
+    start = '**',
+    stop = '**',
+    col = scheme.keyword,
+    keywords = {
+      { '\\*', '\\\\', },
+    },
+  },
+  { -- strong
+    start = '*',
+    stop = '*',
+    col = scheme.keyword,
+    keywords = {
+      { '\\*', '\\\\', },
+    },
+  },
+}
+
+return col
