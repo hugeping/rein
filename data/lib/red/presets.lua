4b8ade9bf44c40ccdb9a1251b6ceaa53ad616bb0
Peter Kosyh <pkosyh@yandex.ru>
2023-10-22 12:37:42 +0300

    red: markdown syntax

--- data/lib/red/presets.lua
+++ data/lib/red/presets.lua
@@ -2,7 +2,7 @@ return {
   {"%.[cChH]$", { ts = 8, spaces_tab = false, trim_spaces = true, syntax = "c" } },
   {"%.cpp$", { ts = 8, spaces_tab = false, trim_spaces = true, syntax = "c" } },
   {"%.lua$", { ts = 2, spaces_tab = true, trim_spaces = true, syntax = "lua" } },
-  {"%.md$", { ts = 2, spaces_tab = true } },
   {"%.go$", { ts = 8, spaces_tab = false, trim_spaces = true } },
   {"%.py$", { ts = 4, spaces_tab = true, trim_spaces = true } },
+  {"%.md$", { ts = 2, spaces_tab = true, trim_spaces = true, syntax = "markdown" } },
 }
--- data/lib/red/syntax/scheme.lua
+++ data/lib/red/syntax/scheme.lua
@@ -1,5 +1,5 @@
 return {
-  keyword = { 122, 112, 72 },
+  keyword = { 102, 102, 22 },
   comment = { 64, 136, 64 },
   string = { 124, 102, 187 },
   number = { 2, 135, 200 },
