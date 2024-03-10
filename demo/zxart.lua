60b822889aa6096cca56581db8843a952469623a
Peter Kosyh <pkosyh@yandex.ru>
2024-03-11 00:54:04 +0300

    zxart: bl added

--- demo/zxart.lua
+++ demo/zxart.lua
@@ -1,4 +1,4 @@
-local blacklist = { [2322] = true, [3315] = true}
+local blacklist = { [2322] = true, [3315] = true, [6352] = true }
 local exclude = "%%D0%%9E%%D0%%B1%%D0%%BD%%D0%%B0%%D0%%B6%%D0%%B5%%D0%%BD%%D0%%BA%%D0%%B0,%%D0%%A1%%D0%%B5%%D0%%BA%%D1%%81,%%D0%%A1%%D0%%B8%%D1%%81%%D1%%8C%%D0%%BA%%D0%%B8"
 require "tiny"
 local SLIDESHOW_DELAY = 4
