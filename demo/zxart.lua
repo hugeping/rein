2fbcd0e42557d00cbc8294582b16bfaa180b93ea
Peter Kosyh <pkosyh@yandex.ru>
2024-03-10 22:53:27 +0300

    zxart: tags

--- demo/zxart.lua
+++ demo/zxart.lua
@@ -1,6 +1,5 @@
 local blacklist = { [2322] = true, }
-local exclude = "%%D0%%9E%%D0%%B1%%D0%%BD%%D0%%B0%%D0%%B6%%D0%%B5%%D0%%BD%%D0%%BA%%D0%%B0,"..
-  "%%D0%%A1%%D0%%B5%%D0%%BA%%D1%%81"
+local exclude = "%%D0%%9E%%D0%%B1%%D0%%BD%%D0%%B0%%D0%%B6%%D0%%B5%%D0%%BD%%D0%%BA%%D0%%B0,%%D0%%A1%%D0%%B5%%D0%%BA%%D1%%81,%%D0%%A1%%D0%%B8%%D1%%81%%D1%%8C%%D0%%BA%%D0%%B8"
 require "tiny"
 local SLIDESHOW_DELAY = 4
 local title_y = 256-60
