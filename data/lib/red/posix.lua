5d39e9523e584ac5c1607521713e3d40c6b09328
Peter Kosyh <pkosyh@yandex.ru>
2024-03-27 22:12:57 +0300

    red: poll fixes

--- data/lib/red/posix.lua
+++ data/lib/red/posix.lua
@@ -24,9 +24,10 @@ function posix.poll(f)
   local fds = ffi.new("struct pollfd[1]")
   fds[0].fd = ffi.C.fileno(f)
   fds[0].events = 1
-  local ret = ffi.C.poll(fds, 1, 2000)
+  local ret = ffi.C.poll(fds, 1, 1000)
   return ret > 0 and bit.band(fds[0].revents, 1) == 1,
-    bit.band(fds[0].revents, 0x8) ~= 8 and
+    ret >= 0 and
+    bit.band(fds[0].revents, 0x8) ~= 0x8 and
     bit.band(fds[0].revents, 0x11) ~= 0x10
 end
 
