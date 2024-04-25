56ca4ed6accddbaed064998ff0b8bdc1e737a107
Peter Kosyh <pkosyh@yandex.ru>
2024-04-25 18:07:46 +0300

    red: use non blocking read

--- data/lib/red/posix.lua
+++ data/lib/red/posix.lua
@@ -1,8 +1,10 @@
-local posix = { sighup = function() end }
+local posix = { sighup = function() end, nonblock = function() end, poll = function() return true, true end }
 
 if PLATFORM == 'Windows' or type(jit) ~= 'table' then
   return posix
 end
+
+posix.have_poll = true
 
 local ffi = require "ffi"
 ffi.cdef[[
@@ -14,10 +16,15 @@ ffi.cdef[[
   };
   int fileno(struct FILE* stream);
   int poll(struct pollfd *fds, unsigned long nfds, int timeout);
+  int fcntl(int fd, int cmd, int arg);
 ]]
 
 function posix.sighup(on)
   ffi.C.signal(13, on and 0 or 1) -- SIGPIPE, SIG_IGN
+end
+
+function posix.nonblock(f, on)
+  ffi.C.fcntl(ffi.C.fileno(f), 4, on and 2048 or 0)
 end
 
 function posix.poll(f)
