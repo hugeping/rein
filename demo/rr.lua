a81042e57415c87f105b6168d1db8b155e626d0d
peter <pkosyh@yandex.ru>
2026-09-13 16:56:18 +0300

    rr: sound fix + border

--- demo/rr.lua
+++ demo/rr.lua
@@ -2154,6 +2154,7 @@ function f_brk(v,k)
     if s==112 then
       if not explode then explode=10 end
       target=target+1
+      border_nr=1
     elseif v.gate then
       if not explode then explode=10 end
       mksnap(v.nr)
@@ -2747,6 +2748,7 @@ function f_end()
 end
 
 function endm()
+  synth.change(1, 0, synth.NOTE_OFF, 0)
   ship.x=64+cos(ship.h)*4
   ship.h=ship.h+rnd(0.01)
   ship.v=ship.v+rnd(0.005)
