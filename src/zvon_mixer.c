d24d8fc5cff25309162f1759314cb83f925024c5
peter <pkosyh@yandex.ru>
2026-09-13 15:16:50 +0300

    zvon declick

--- src/zvon_mixer.c
+++ src/zvon_mixer.c
@@ -2,13 +2,30 @@
 
 #include <math.h>
 #include <stdlib.h>
+#include <string.h>
 #include "zvon_mixer.h"
 
 void sfx_box_set_vol(struct sfx_box *box, double vol) {
     box->vol = vol;
 }
 
+static void stack_free(struct sfx_box *stack, int *size) {
+    for (int i = 0; i < *size; i++) {
+        if (stack[i].proto->free) {
+            stack[i].proto->free(stack[i].state);
+        }
+        free(stack[i].state);
+        stack[i].state = NULL;
+    }
+    *size = 0;
+}
+
 void chan_set_on(struct chan_state *c, int is_on) {
+    if (!is_on) {
+        stack_free(c->stack, &c->stack_size);
+        stack_free(c->fade_stack, &c->fade_stack_size);
+        c->fade = 0;
+    }
     c->is_on = is_on;
 }
 
@@ -22,14 +39,19 @@ void chan_set_pan(struct chan_state *c, double pan) {
     c->pan_right = sqrt(pan);
 }
 
+/* the old stack is kept for a few ms so an instrument change does not click */
+#define CHAN_FADE_STEP (130.0 / SR)
+
 void chan_drop(struct chan_state *c) {
-    for (int i = 0; i < c->stack_size; i++) {
-        if (c->stack[i].proto->free) {
-            c->stack[i].proto->free(c->stack[i].state);
-        }
-        free(c->stack[i].state);
-        c->stack[i].state = NULL;
+    if (!c->stack_size) {
+        return;
     }
+    if (c->fade_stack_size) {
+        stack_free(c->fade_stack, &c->fade_stack_size);
+    }
+    memcpy(c->fade_stack, c->stack, sizeof(c->stack[0]) * c->stack_size);
+    c->fade_stack_size = c->stack_size;
+    c->fade = 1.0;
     c->stack_size = 0;
 }
 
@@ -48,7 +70,7 @@ struct sfx_box *chan_push(struct chan_state *c, struct sfx_proto *proto) {
     return NULL;
 }
 
-static void chan_process(struct sfx_box *stack, int stack_size, double *l, double *r) {
+static void stack_process(struct sfx_box *stack, int stack_size, double *l, double *r) {
     for (int i = 0; i < stack_size; i++) {
         struct sfx_box *box = &stack[i];
         if (box->proto->stereo) {
@@ -62,6 +84,25 @@ static void chan_process(struct sfx_box *stack, int stack_size, double *l, doubl
     }
 }
 
+static void chan_process(struct chan_state *c, double *l, double *r) {
+    double l1 = 0, r1 = 0;
+    stack_process(c->stack, c->stack_size, &l1, &r1);
+    if (c->fade > 0) {
+        double l2 = 0, r2 = 0;
+        stack_process(c->fade_stack, c->fade_stack_size, &l2, &r2);
+        *l = l1 * (1 - c->fade) + l2 * c->fade;
+        *r = r1 * (1 - c->fade) + r2 * c->fade;
+        c->fade -= CHAN_FADE_STEP;
+        if (c->fade <= 0) {
+            c->fade = 0;
+            stack_free(c->fade_stack, &c->fade_stack_size);
+        }
+    } else {
+        *l = l1;
+        *r = r1;
+    }
+}
+
 void mix_init(struct chan_state *chans, int num_chans) {
     for (int i = 0; i < num_chans; i++) {
         struct chan_state *c = &chans[i];
@@ -69,6 +110,8 @@ void mix_init(struct chan_state *chans, int num_chans) {
         chan_set_vol(c, 0);
         chan_set_pan(c, 0);
         c->stack_size = 0;
+        c->fade_stack_size = 0;
+        c->fade = 0;
     }
 }
 double mix_process(struct chan_state *chans, int num_chans, double vol, float *samps, int num_samps) {
@@ -79,7 +122,7 @@ double mix_process(struct chan_state *chans, int num_chans, double vol, float *s
             struct chan_state *c = &chans[i];
             if (c->is_on) {
                 double l = 0, r = 0;
-                chan_process(c->stack, c->stack_size, &l, &r);
+                chan_process(c, &l, &r);
                 left += c->vol * l * c->pan_left;
                 right += c->vol * r * c->pan_right;
             }
