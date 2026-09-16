2ba1e16ef95047b627d4f9036039afe4af7f8946
big-pickle <big-pickle@opencode.ai>
2026-09-16 09:08:02 +0300

    revert not threaded mode

--- make.sh
+++ make.sh
@@ -1,4 +1,3 @@
-CFLAGS="`pkg-config --cflags sdl3` `pkg-config --cflags lua5.4`"
-LDFLAGS="`pkg-config --libs sdl3` `pkg-config --libs lua5.4` -lm"
-gcc -fsanitize=address,undefined -ggdb -O0 -Wall -O3 src/*.c src/sdl3/platform.c $CFLAGS $LDFLAGS -o rein -DVERSION=\"`date +%y%m%d`\"
-rm -f *.o
+CFLAGS="`pkg-config --cflags sdl2` `pkg-config --cflags luajit`"
+LDFLAGS="`pkg-config --libs sdl2` `pkg-config --libs luajit` -lm"
+gcc -Wall -O3 src/*.c src/sdl2/platform.c $CFLAGS $LDFLAGS -o rein -DVERSION=\"`date +%y%m%d`\"
